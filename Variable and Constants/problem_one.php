94966a163597a99f5bf09b6f075279d86acb321e
Ahmed-Wassim <ahmedwassim317@gmail.com>
2022-12-21 23:09:57 +0200

    This is the solve of the problem one in course elzero php

---
+++ Variable and Constants/problem_one.php
@@ -0,0 +1,8 @@
+<?php      
+    $a = 100;     
+    $b = 200;     
+    $c = 100;     
+    /* b > a = true  and     a identical to c = true      and a plus c equal = true */
+    if ($b > $a && $a === $c && ($a + $c) == $b) { 
+                echo "Yes";    
+        } 
