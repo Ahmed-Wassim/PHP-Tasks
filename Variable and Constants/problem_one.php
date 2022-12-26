4b85650862d1058331b65fa816be760b54ca4e8c
Ahmed-Wassim <ahmedwassim317@gmail.com>
2022-12-26 19:01:38 +0200

    Removed file

--- Variable and Constants/problem_one.php
+++
@@ -1,8 +0,0 @@
-<?php      
-    $a = 100;     
-    $b = 200;     
-    $c = 100;     
-    /* b > a = true  and     a identical to c = true      and a plus c equal = true */
-    if ($b > $a && $a === $c && ($a + $c) == $b) { 
-                echo "Yes";    
-        } 
